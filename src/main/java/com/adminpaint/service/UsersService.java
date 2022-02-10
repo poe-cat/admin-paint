f4d2ae085f37356fcffd3c84c009c44b7d1d63a5
Kasia <katarzyna.tramea@gmail.com>
2022-02-10 20:29:44 +0100

    create repository, service and controller for "Users" (login & register)

---
+++ src/main/java/com/adminpaint/service/UsersService.java
@@ -0,0 +1,34 @@
+package com.adminpaint.service;
+
+import com.adminpaint.model.Users;
+import com.adminpaint.repository.UsersRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsersService {
+
+    private final UsersRepository usersRepository;
+
+    public UsersService(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+
+    public Users registerUser(String login, String password, String email) {
+
+        if(login == null || password == null) {
+            return null;
+        }
+        else {
+            Users users = new Users();
+            users.setLogin(login);
+            users.setPassword(password);
+            users.setEmail(email);
+
+            return usersRepository.save(users);
+        }
+    }
+
+    public Users authenticate(String login, String password) {
+        return usersRepository.findByLoginAndPassword(login, password).orElse(null);
+    }
+}
