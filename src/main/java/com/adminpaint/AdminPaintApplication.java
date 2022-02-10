8561bacd265df432c01daac8205179a036e2e3ff
Kasia <katarzyna.tramea@gmail.com>
2022-02-10 20:09:59 +0100

    first commit

---
+++ src/main/java/com/adminpaint/AdminPaintApplication.java
@@ -0,0 +1,13 @@
+package com.adminpaint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminPaintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminPaintApplication.class, args);
+	}
+
+}
