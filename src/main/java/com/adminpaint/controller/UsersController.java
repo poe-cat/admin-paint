982e9679ab4b56e6be753f28707486910e63c7e7
Kasia <katarzyna.tramea@gmail.com>
2022-02-12 15:00:30 +0100

    add PostMapping for login and register, update model attribute and add constructor with UsersService

--- src/main/java/com/adminpaint/controller/UsersController.java
+++ src/main/java/com/adminpaint/controller/UsersController.java
@@ -1,18 +1,51 @@
 package com.adminpaint.controller;
 
+import com.adminpaint.model.Users;
+import com.adminpaint.service.UsersService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UsersController {
 
-    @GetMapping("register")
-    public String getRegisterPage() {
+    private final UsersService usersService;
+
+    public UsersController(UsersService usersService) {
+        this.usersService = usersService;
+    }
+
+    @GetMapping("/register")
+    public String getRegisterPage(Model model) {
+        model.addAttribute("registerRequest", new Users());
         return "register_page";
     }
 
     @GetMapping("/login")
-    public String getLoginPage() {
+    public String getLoginPage(Model model) {
+        model.addAttribute("loginRequest", new Users());
         return "login_page";
+    }
+
+    @PostMapping("/register")
+    public String register(@ModelAttribute Users users) {
+        System.out.println("register request: " + users);
+        Users registeredUser = usersService.registerUser(users.getLogin(), users.getPassword(), users.getEmail());
+        return registeredUser == null ? "error_page" : "redirect:/login";
+    }
+
+    @PostMapping("/login")
+    public String login(@ModelAttribute Users users, Model model) {
+        System.out.println("login request: " + users);
+        Users authenticated = usersService.authenticate(users.getLogin(), users.getPassword());
+        if(authenticated != null) {
+            model.addAttribute("userLogin", authenticated.getLogin());
+            return "personal_page";
+        }
+        else {
+            return "error_page";
+        }
     }
 }
