197cddd4d5d0b50f57af685d548fdbf7a54427cc
Kasia <katarzyna.tramea@gmail.com>
2022-02-24 17:06:22 +0100

    create ClientRepository.java

---
+++ src/main/java/com/adminpaint/repository/ClientRepository.java
@@ -0,0 +1,9 @@
+package com.adminpaint.repository;
+
+import com.adminpaint.model.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientRepository extends JpaRepository<Client, Integer> {
+
+
+}
