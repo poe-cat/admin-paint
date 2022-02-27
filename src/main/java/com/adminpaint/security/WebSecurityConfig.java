1376e1c488389a010dcb5f646ba80b2138e6c7bb
Kasia <katarzyna.tramea@gmail.com>
2022-02-27 17:30:58 +0100

    update security: custom login form, antMatchers, update query ("username" instead of "login"), add BCryptPasswordEncoder

--- src/main/java/com/adminpaint/security/WebSecurityConfig.java
+++ src/main/java/com/adminpaint/security/WebSecurityConfig.java
@@ -1,9 +1,11 @@
 package com.adminpaint.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -17,24 +19,37 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private DataSource dataSource;
 
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .antMatchers("/resources/**", "/static/**");
+    }
+
     @Autowired
     public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
         auth.jdbcAuthentication().passwordEncoder(new BCryptPasswordEncoder())
                 .dataSource(dataSource)
-                .usersByUsernameQuery("select login, password, enabled from users_table where login=?")
-                .authoritiesByUsernameQuery("select login, role from users_table where login=?");
+                .usersByUsernameQuery("select username, password, enabled from users_table where username=?")
+                .authoritiesByUsernameQuery("select username, role from users_table where username=?");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/edit/*", "/delete/*").hasRole("ADMIN")
-                .anyRequest().authenticated()
+                .antMatchers("/login", "/register")
+                .permitAll()
+                .antMatchers("*/new/*", "*/edit/*", "*/delete/*").hasRole("ADMIN")
+                .antMatchers("/commissions", "/clients").hasAnyRole("ADMIN", "USER")
                 .and()
-                .formLogin().permitAll()
-                .and()
-                .logout().permitAll()
-                .and()
-                .exceptionHandling().accessDeniedPage("/403_error");
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/")
+                        .failureUrl("/login?error=true")
+                );
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
